65a000973cdd571812c5da961d642463d486074c
dopi <dominikajoannapietrzyk@gmail.com>
2021-07-16 15:59:38 +0200

    Create Mapper for Client and Loan.

--- src/main/java/loan/application/SpringBootLoanApplication/api/mapper/ClientMapper.java
+++ src/main/java/loan/application/SpringBootLoanApplication/api/mapper/ClientMapper.java
@@ -1,7 +1,16 @@
 package loan.application.SpringBootLoanApplication.api.mapper;
 
+import loan.application.SpringBootLoanApplication.api.model.ClientDTO;
+import loan.application.SpringBootLoanApplication.domain.Client;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface ClientMapper {
+
+    ClientMapper INSTANCE = Mappers.getMapper(ClientMapper.class);
+
+    ClientDTO clientToClientDTO(Client client);
+
+    Client clientDtoToClient(ClientDTO clientDTO);
 }
--- src/main/java/loan/application/SpringBootLoanApplication/api/mapper/LoanMapper.java
+++ src/main/java/loan/application/SpringBootLoanApplication/api/mapper/LoanMapper.java
@@ -1,7 +1,17 @@
 package loan.application.SpringBootLoanApplication.api.mapper;
 
+import loan.application.SpringBootLoanApplication.api.model.LoanDTO;
+import loan.application.SpringBootLoanApplication.domain.Loan;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface LoanMapper {
+
+    LoanMapper INSTANCE = Mappers.getMapper(LoanMapper.class);
+
+    LoanDTO loanToLoanDTO(Loan loan);
+
+    Loan loanDtoToLoan(LoanDTO loanDTO);
+
 }
