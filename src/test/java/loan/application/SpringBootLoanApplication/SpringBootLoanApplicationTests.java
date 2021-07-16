b49f3e9c0ac3544e2c9b8e6eff61833c3f75bb5a
dopi <dominikajoannapietrzyk@gmail.com>
2021-07-16 12:54:18 +0200

    Basic structure and classes.

---
+++ src/test/java/loan/application/SpringBootLoanApplication/SpringBootLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package loan.application.SpringBootLoanApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
